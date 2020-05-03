19e511b63eb2b3fc3d5694d3285e09bdf88e3712
DanielGeek <daniel30081990@gmail.com>
2020-05-03 09:49:30 -0400

    Borrar estudiantes

--- studen_crud/resources/views/student/index.blade.php
+++ studen_crud/resources/views/student/index.blade.php
@@ -33,11 +33,30 @@
                         Edit
                     </a>
                 </td>
-                <td></td>
+                <td>
+                    <form method="post" class="delete_form" action="{{action('StudentController@destroy', $row['id'])}}">
+                        {{csrf_field()}}
+                        <input type="hidden" name="_method" value="DELETE" />
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
             </tr>
             @endforeach
         </table>
     </div>
 </div>
-
+<script>
+    $(document).ready(function(){
+        $('.delete_form').on('submit', function(){
+            if(confirm("Are you sure you want to delete it?"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        });
+    });
+</script>
 @endSection
