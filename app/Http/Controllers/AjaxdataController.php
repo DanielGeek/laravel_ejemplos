e7e073f2383102ad9d978c2164ba1e4993b56a4c
DanielGeek <daniel30081990@gmail.com>
2020-05-03 12:52:24 -0400

    Insertar Data usundo Ajax en Laravel con Datatables

--- app/Http/Controllers/AjaxdataController.php
+++ app/Http/Controllers/AjaxdataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
 use App\Student;
 use Datatables;
@@ -18,5 +19,40 @@ class AjaxdataController extends Controller
     {
         $students = Student::select('first_name', 'last_name');
         return Datatables::of($students)->make(true);
+    }
+
+    function postdata(Request $request)
+    {
+        $validation = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name'  => 'required',
+        ]);
+
+        $error_array = array();
+        $success_output = '';
+        if ($validation->fails())
+        {
+            foreach($validation->messages()->getMessages() as $field_name => $messages)
+            {
+                $error_array[] = $messages;
+            }
+        }
+        else
+        {
+            if($request->get('button_action') == "insert")
+            {
+                $student = new Student([
+                    'first_name'    =>  $request->get('first_name'),
+                    'last_name'     =>  $request->get('last_name')
+                ]);
+                $student->save();
+                $success_output = '<div class="alert alert-success">Data Inserted</div>';
+            }
+        }
+        $output = array(
+            'error'     =>  $error_array,
+            'success'   =>  $success_output
+        );
+        echo json_encode($output);
     }
 }
