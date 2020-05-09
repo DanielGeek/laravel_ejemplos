0effb04d2576c711d66abb86a4fd0d24cde2e405
DanielGeek <daniel30081990@gmail.com>
2020-05-09 01:14:42 -0400

    Plugin Tags Input

--- cms/resources/views/plantilla.blade.php
+++ cms/resources/views/plantilla.blade.php
@@ -9,19 +9,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.6.0/css/all.min.css">
     <!-- bootstrap 4 -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- admin lte -->
+    <!-- OverlayScrollbars -->
     <link rel="stylesheet" href="{{ url('/') }}/css/plugins/OverlayScrollbars.min.css">
+    <!-- tagsinput -->
+    <link rel="stylesheet" href="{{ url('/') }}/css/plugins/tagsinput.css">
     <!-- admin lte -->
     <link rel="stylesheet" href="{{ url('/') }}/css/plugins/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-    
 
     <!-- jQuery -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="{{ url('/') }}/js/plugins/overlayScrollbars.min.js"></script>
+    <script src="{{ url('/') }}/js/plugins/tagsinput.js"></script>
     <script src="{{ url('/') }}/js/plugins/adminlte.js"></script>
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
